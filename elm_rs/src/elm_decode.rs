934325a068a48f63d55c1abd380b25f356392c23
Heliozoa <daniel.x.martinez@helsinki.fi>
2023-12-08 04:50:43 +0200

    Cleanup

--- elm_rs/src/elm_decode.rs
+++ elm_rs/src/elm_decode.rs
@@ -27,7 +27,7 @@ where
     T: Elm + ElmDecode,
 {
     fn decoder_type() -> String {
-        ::std::format!("(Json.Decode.index 0 ({}))", T::decoder_type(),)
+        ::std::format!("(Json.Decode.index 0 ({}))", T::decoder_type())
     }
 
     fn decoder_definition() -> Option<String> {
@@ -153,7 +153,7 @@ systemTimeDecoder =
 }
 
 macro_rules! impl_builtin {
-    ($rust_type: ty, $elm_type: expr, $elm_decoder: expr, $elm_encoder: expr) => {
+    ($rust_type: ty, $elm_type: expr, $elm_decoder: expr) => {
         impl ElmDecode for $rust_type {
             fn decoder_type() -> String {
                 $elm_decoder.to_string()
@@ -167,7 +167,7 @@ macro_rules! impl_builtin {
 }
 
 macro_rules! impl_builtin_container {
-    ($rust_type: ty, $elm_name: expr, $elm_decoder: expr, $elm_encoder: expr) => {
+    ($rust_type: ty, $elm_name: expr, $elm_decoder: expr) => {
         impl<T: Elm + ElmDecode> ElmDecode for $rust_type {
             fn decoder_type() -> String {
                 ::std::format!("{} ({})", $elm_decoder, T::decoder_type())
@@ -211,240 +211,75 @@ macro_rules! impl_builtin_ptr {
 impl_builtin_ptr!(&'_ T);
 impl_builtin_ptr!(&'_ mut T);
 impl_builtin_ptr!(std::sync::Arc<T>);
-impl_builtin!(
-    std::sync::atomic::AtomicBool,
-    "Bool",
-    "Json.Decode.bool",
-    "Json.Encode.bool"
-);
-impl_builtin!(
-    std::sync::atomic::AtomicU8,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::sync::atomic::AtomicU16,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::sync::atomic::AtomicU32,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::sync::atomic::AtomicU64,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::sync::atomic::AtomicUsize,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::sync::atomic::AtomicI8,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::sync::atomic::AtomicI16,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::sync::atomic::AtomicI32,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::sync::atomic::AtomicI64,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::sync::atomic::AtomicIsize,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
+impl_builtin!(std::sync::atomic::AtomicBool, "Bool", "Json.Decode.bool");
+impl_builtin!(std::sync::atomic::AtomicU8, "Int", "Json.Decode.int");
+impl_builtin!(std::sync::atomic::AtomicU16, "Int", "Json.Decode.int");
+impl_builtin!(std::sync::atomic::AtomicU32, "Int", "Json.Decode.int");
+impl_builtin!(std::sync::atomic::AtomicU64, "Int", "Json.Decode.int");
+impl_builtin!(std::sync::atomic::AtomicUsize, "Int", "Json.Decode.int");
+impl_builtin!(std::sync::atomic::AtomicI8, "Int", "Json.Decode.int");
+impl_builtin!(std::sync::atomic::AtomicI16, "Int", "Json.Decode.int");
+impl_builtin!(std::sync::atomic::AtomicI32, "Int", "Json.Decode.int");
+impl_builtin!(std::sync::atomic::AtomicI64, "Int", "Json.Decode.int");
+impl_builtin!(std::sync::atomic::AtomicIsize, "Int", "Json.Decode.int");
 impl_builtin_map!(std::collections::BTreeMap<String,T>);
-impl_builtin_container!(
-    std::collections::BTreeSet<T>,
-    "List",
-    "Json.Decode.list",
-    "Json.Encode.list"
-);
+impl_builtin_container!(std::collections::BTreeSet<T>, "List", "Json.Decode.list");
 impl_builtin_ptr!(Box<T>);
 impl_builtin_ptr!(std::cell::Cell<T>);
 impl_builtin_map!(std::collections::HashMap<String,T>);
-impl_builtin_container!(
-    std::collections::HashSet<T>,
-    "List",
-    "Json.Decode.list",
-    "Json.Encode.list"
-);
+impl_builtin_container!(std::collections::HashSet<T>, "List", "Json.Decode.list");
 // todo ipaddrs
-impl_builtin_container!(
-    std::collections::LinkedList<T>,
-    "List",
-    "Json.Decode.list",
-    "Json.Encode.list"
-);
+impl_builtin_container!(std::collections::LinkedList<T>, "List", "Json.Decode.list");
 impl_builtin_ptr!(std::sync::Mutex<T>);
-impl_builtin!(
-    std::num::NonZeroU8,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::num::NonZeroU16,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::num::NonZeroU32,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::num::NonZeroU64,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::num::NonZeroU128,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::num::NonZeroUsize,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::num::NonZeroI8,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::num::NonZeroI16,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::num::NonZeroI32,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::num::NonZeroI64,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::num::NonZeroI128,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin!(
-    std::num::NonZeroIsize,
-    "Int",
-    "Json.Decode.int",
-    "Json.Encode.int"
-);
-impl_builtin_container!(
-    Option<T>,
-    "Maybe",
-    "Json.Decode.nullable",
-    "Maybe.withDefault Json.Encode.null << Maybe.map"
-);
-impl_builtin!(
-    std::path::Path,
-    "String",
-    "Json.Decode.string",
-    "Json.Encode.string"
-);
-impl_builtin!(
-    std::path::PathBuf,
-    "String",
-    "Json.Decode.string",
-    "Json.Encode.string"
-);
+impl_builtin!(std::num::NonZeroU8, "Int", "Json.Decode.int");
+impl_builtin!(std::num::NonZeroU16, "Int", "Json.Decode.int");
+impl_builtin!(std::num::NonZeroU32, "Int", "Json.Decode.int");
+impl_builtin!(std::num::NonZeroU64, "Int", "Json.Decode.int");
+impl_builtin!(std::num::NonZeroU128, "Int", "Json.Decode.int");
+impl_builtin!(std::num::NonZeroUsize, "Int", "Json.Decode.int");
+impl_builtin!(std::num::NonZeroI8, "Int", "Json.Decode.int");
+impl_builtin!(std::num::NonZeroI16, "Int", "Json.Decode.int");
+impl_builtin!(std::num::NonZeroI32, "Int", "Json.Decode.int");
+impl_builtin!(std::num::NonZeroI64, "Int", "Json.Decode.int");
+impl_builtin!(std::num::NonZeroI128, "Int", "Json.Decode.int");
+impl_builtin!(std::num::NonZeroIsize, "Int", "Json.Decode.int");
+impl_builtin_container!(Option<T>, "Maybe", "Json.Decode.nullable");
+impl_builtin!(std::path::Path, "String", "Json.Decode.string");
+impl_builtin!(std::path::PathBuf, "String", "Json.Decode.string");
 // todo phantomdata
 impl_builtin_ptr!(std::rc::Rc<T>);
 impl_builtin_ptr!(std::cell::RefCell<T>);
 impl_builtin_ptr!(std::sync::RwLock<T>);
 // todo socketaddrs
-impl_builtin!(String, "String", "Json.Decode.string", "Json.Encode.string");
-impl_builtin_container!(Vec<T>, "List", "Json.Decode.list", "Json.Encode.list");
-impl_builtin_container!([T], "List", "Json.Decode.list", "Json.Encode.list");
-impl_builtin!(bool, "Bool", "Json.Decode.bool", "Json.Encode.bool");
-impl_builtin!(u8, "Int", "Json.Decode.int", "Json.Encode.int");
-impl_builtin!(u16, "Int", "Json.Decode.int", "Json.Encode.int");
-impl_builtin!(u32, "Int", "Json.Decode.int", "Json.Encode.int");
-impl_builtin!(u64, "Int", "Json.Decode.int", "Json.Encode.int");
-impl_builtin!(u128, "Int", "Json.Decode.int", "Json.Encode.int");
-impl_builtin!(usize, "Int", "Json.Decode.int", "Json.Encode.int");
-impl_builtin!(i8, "Int", "Json.Decode.int", "Json.Encode.int");
-impl_builtin!(i16, "Int", "Json.Decode.int", "Json.Encode.int");
-impl_builtin!(i32, "Int", "Json.Decode.int", "Json.Encode.int");
-impl_builtin!(i64, "Int", "Json.Decode.int", "Json.Encode.int");
-impl_builtin!(i128, "Int", "Json.Decode.int", "Json.Encode.int");
-impl_builtin!(isize, "Int", "Json.Decode.int", "Json.Encode.int");
-impl_builtin!(f32, "Float", "Json.Decode.float", "Json.Encode.float");
-impl_builtin!(f64, "Float", "Json.Decode.float", "Json.Encode.float");
-impl_builtin!(str, "String", "Json.Decode.string", "Json.Encode.string");
+impl_builtin!(String, "String", "Json.Decode.string");
+impl_builtin_container!(Vec<T>, "List", "Json.Decode.list");
+impl_builtin_container!([T], "List", "Json.Decode.list");
+impl_builtin!(bool, "Bool", "Json.Decode.bool");
+impl_builtin!(u8, "Int", "Json.Decode.int");
+impl_builtin!(u16, "Int", "Json.Decode.int");
+impl_builtin!(u32, "Int", "Json.Decode.int");
+impl_builtin!(u64, "Int", "Json.Decode.int");
+impl_builtin!(u128, "Int", "Json.Decode.int");
+impl_builtin!(usize, "Int", "Json.Decode.int");
+impl_builtin!(i8, "Int", "Json.Decode.int");
+impl_builtin!(i16, "Int", "Json.Decode.int");
+impl_builtin!(i32, "Int", "Json.Decode.int");
+impl_builtin!(i64, "Int", "Json.Decode.int");
+impl_builtin!(i128, "Int", "Json.Decode.int");
+impl_builtin!(isize, "Int", "Json.Decode.int");
+impl_builtin!(f32, "Float", "Json.Decode.float");
+impl_builtin!(f64, "Float", "Json.Decode.float");
+impl_builtin!(str, "String", "Json.Decode.string");
 
 #[cfg(feature = "uuid")]
-impl_builtin!(
-    uuid::Uuid,
-    "String",
-    "Json.Decode.string",
-    "Json.Encode.string"
-);
+impl_builtin!(uuid::Uuid, "String", "Json.Decode.string");
 
 #[cfg(feature = "chrono")]
-impl_builtin!(
-    chrono::NaiveTime,
-    "String",
-    "Json.Decode.string",
-    "Json.Encode.string"
-);
+impl_builtin!(chrono::NaiveTime, "String", "Json.Decode.string");
 #[cfg(feature = "chrono")]
-impl_builtin!(
-    chrono::NaiveDate,
-    "String",
-    "Json.Decode.string",
-    "Json.Encode.string"
-);
+impl_builtin!(chrono::NaiveDate, "String", "Json.Decode.string");
 #[cfg(feature = "chrono")]
-impl_builtin!(
-    chrono::NaiveDateTime,
-    "String",
-    "Json.Decode.string",
-    "Json.Encode.string"
-);
+impl_builtin!(chrono::NaiveDateTime, "String", "Json.Decode.string");
 #[cfg(feature = "chrono")]
 impl<T: chrono::TimeZone> ElmDecode for chrono::DateTime<T> {
     fn decoder_type() -> String {
